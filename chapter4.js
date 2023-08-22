b4003102c6668eeb61e3bae948706b671f3a3101
lukachochua <lukachochua@gmail.com>
2023-08-22 21:43:58 +0400

    Chapter 4: reverse array and list exercises

--- chapter4.js
+++ chapter4.js
@@ -34,3 +34,80 @@ console.log(sum(range(1, 10)));
 
 // Reversing an array
 
+let newArray = [];
+
+
+function reverseArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        newArray.unshift(array[i]);
+    }
+    return newArray;
+}
+
+function reverseArrayInPlace(array) {
+    for (let i = 0; i < Math.floor(array.length / 2); i++) {
+        let index = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = index;
+    }
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+let arrayValue = [1, 2, 3, 4, 5, 6];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+
+
+// List
+
+function arrayToList(array) {
+    let list;
+    for (let i = array.length - 1; i >= 0; i--) {
+        list = { value: array[i], rest: list };
+    }
+    return list;
+}
+
+function listToArray(list) {
+    let array = [];
+
+    for (let i = list; i; i = i.rest) {
+        array.push(i.value);
+    }
+    return array;
+}
+
+function prepend(element, list) {
+    return { value: element, rest: list };
+}
+
+function nth(list, n) {
+    if (!list) {
+        return undefined;
+    }
+
+    let node = list;
+
+    for (let i = 0; i < n; i++) {
+        if (!node.rest) {
+            return undefined;
+        }
+
+        node = node.rest;
+    }
+
+    return node.value;
+}
+
+function recursiveNth(list, n) {
+    if (!list) {
+        return undefined; 
+    } else if (n === 0) {
+        return list.value; 
+    } else {
+        return recursiveNth(list.rest, n - 1); 
+    }
+}
+
+
+
