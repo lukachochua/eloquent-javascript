8ec43bcc54b7f7f9787d6ce46e9eb26268cac54f
lukachochua <lukachochua@gmail.com>
2023-08-30 22:48:10 +0400

    Chapter 5: dominant writing direction

--- chapter5.js
+++ chapter5.js
@@ -20,7 +20,7 @@ function loop(value, testFn, updateFn, bodyFn) {
 loop(3, n => n > 0, n => n - 1, console.log);
 
 
-// Everythingg
+// Everything
 
 function every(array, test) {
     for (let i = 0; i < array.length; i++) {
@@ -41,3 +41,47 @@ console.log(every([2, 4, 16], n => n < 10));
 // → false
 console.log(every([], function (n) { return n < 10; }));
   // → true
+
+// Dominant Writing Direction
+
+function dominantDirection(text) {
+    function characterScript(code) {
+      for (let script of SCRIPTS) {
+        if (script.ranges.some(([from, to]) => code >= from && code < to)) {
+          return script;
+        }
+      }
+      return null;
+    }
+  
+    function countBy(items, groupName) {
+      let counts = [];
+      for (let item of items) {
+        let name = groupName(item);
+        let known = counts.findIndex(c => c.name == name);
+        if (known == -1) {
+          counts.push({ name, count: 1 });
+        } else {
+          counts[known].count++;
+        }
+      }
+      return counts;
+    }
+  
+    function dominantDirectionFromCounts(counts) {
+      let dominant = counts.reduce((a, b) => (a.count > b.count ? a : b));
+      return dominant.name;
+    }
+  
+    let scriptCounts = countBy(text, char => {
+      let script = characterScript(char.codePointAt(0));
+      return script ? script.direction : 'none';
+    }).filter(({ name }) => name !== 'none');
+  
+    return dominantDirectionFromCounts(scriptCounts);
+  }
+  
+  console.log(dominantDirection("Hello!"));
+  // → ltr
+  console.log(dominantDirection("Hey, مساء الخير"));
+  // → rtl
